f1348e91ced91741eb10158bf4b3787eec057334
Arthur Vinícius <40926020+arthurvinice@users.noreply.github.com>
2023-12-05 14:00:40 -0300

    adicionado funcionalidade ao apertar enter

--- login.py
+++ login.py
@@ -1,16 +1,16 @@
 from guizero import App, Text, TextBox, PushButton, Window
 
-#Funcao para verificação do login
+#Funcao para verificar o login
 def verificar_login(usuario, senha):
     with open('cadastros.txt', 'r+', encoding='utf-8') as cadastros:
         nomes = cadastros.readlines()
-        for linha in nomes:
-            user, password = linha.strip().split(',')
-            if user == usuario and password == senha:
+        for dados in nomes:
+            linha = dados.strip().split(',')
+            if len(linha) == 2 and linha[0] == usuario and linha[1] == senha:
                 return True
     return False
 
-#Funcao de login
+#Funcaao de login
 def fazer_login():
     usuario_entrada = campo_user.value
     senha_entrada = campo_senha.value
@@ -25,7 +25,19 @@ def cancelar():
     print(f"Operação cancelada pelo usuário.")
     app.destroy()
 
-#Funcao para segunda janela
+#Funcao para fechar o app quando fechar a janela principal
+def fechar_app():
+    app.destroy()
+
+#Funcao para apertar o enter
+def press_enter(evento):
+    if campo_user is not None and campo_senha is not None:
+        if evento.key == '\r':
+            fazer_login()
+    else:
+        print('ACESSO NEGADO')
+
+#Funcao para segunda janela (apos login)
 def abrir_jan_principal():
     msg_acesso.value = "ACESSO LIBERADO!"
     janela_principal.show()
@@ -36,15 +48,17 @@ app = App(title="LOGIN", layout="grid", height=130, width=180)
 #Janela principal para o admin
 janela_principal = Window(app, title='ADMINISTRADOR', width=400, height=300)
 janela_principal.hide()
+janela_principal.when_closed = fechar_app
 
 #Area de login
 msg_usuario = Text(app, text="Usuário: ",grid=[0,0])
 campo_user = TextBox(app,grid=[1,0],width="fill")
 msg_senha = Text(app, text="Senha: ",grid=[0,1])
 campo_senha = TextBox(app,grid=[1,1],width="fill")
-button = PushButton(app, text="Entrar", command=fazer_login,grid=[0,3])
+campo_senha.when_key_pressed = press_enter
+button_entrar = PushButton(app, text="Entrar",command=fazer_login,grid=[0,3])
 Text(app, grid=[1,2])
-button = PushButton(app, text="Cancelar", command=cancelar,grid=[1,3])
+button_cancelar = PushButton(app, text="Cancelar", command=cancelar,grid=[1,3])
 
 #Mensagem de acesso ao login admin
 msg_acesso = Text(janela_principal, text="")
